1bcf7bbfbd544c7a6d94fba3eb4ffdafc9a238aa
José Pedro Dias <jose.pedro.dias@gmail.com>
2019-01-26 11:39:30 +0000

    validate opcode arguments

--- hrm.js
+++ hrm.js
@@ -21,6 +21,17 @@ function randomArray(arr) {
   return arr[randomInt(n)];
 }
 
+function isNumber(n) {
+  return typeof n === 'number';
+}
+
+function isInteger(n) {
+  if (typeof n !== 'number') {
+    return false;
+  }
+  return n === Math.floor(n);
+}
+
 /*
 IN  .....* ->
 OUT        -> *.....
@@ -30,34 +41,73 @@ const MAX_STEPS = 20;
 
 const OPS = {
   inbox: function() {
+    if (this.in.length === 0) {
+      throw 'inbox is empty!';
+    }
     this.hand = this.in.pop();
   },
   outbox: function() {
+    if (this.hand === undefined) {
+      throw 'outbox requires hand!';
+    }
     this.out.unshift(this.hand);
   },
   copyfrom: function(index) {
+    if (!isInteger(index)) {
+      throw 'copyfrom argument 0 (index) must be an integer!';
+    }
     this.hand = this.cells[index];
   },
   copyto: function(index) {
+    if (!isInteger(index)) {
+      throw 'copyto argument 0 (index) must be an integer!';
+    }
     this.cells[index] = this.hands;
   },
   jump: function(lineNo) {
+    if (!isInteger(lineNo)) {
+      throw 'jump argument 0 (lineNo) must be an integer!';
+    }
     this.lineNo = lineNo - 1;
   },
   jump0: function(lineNo) {
+    if (!isInteger(lineNo)) {
+      throw 'jump0 argument 0 (lineNo) must be an integer!';
+    }
     if (this.hand === 0) {
       this.lineNo = lineNo - 1;
     }
   },
   jumpneg: function(lineNo) {
+    if (!isInteger(lineNo)) {
+      throw 'jumpneg argument 0 (lineNo) must be an integer!';
+    }
     if (this.hand < 0) {
       this.lineNo = lineNo - 1;
     }
   },
   add: function(index) {
+    if (!isInteger(index)) {
+      throw 'add argument 0 (index) must be an integer!';
+    }
+    if (!isNumber(this.hand)) {
+      throw 'hand must have a number!';
+    }
+    if (!isNumber(this.cells[index])) {
+      throw `cell #${index} must have a number!`;
+    }
     this.hand += this.cells[index];
   },
   sub: function(index) {
+    if (!isInteger(index)) {
+      throw 'add argument 0 (index) must be an integer!';
+    }
+    if (!isNumber(this.hand)) {
+      throw 'hand must have a number!';
+    }
+    if (!isNumber(this.cells[index])) {
+      throw `cell #${index} must have a number!`;
+    }
     this.hand -= this.cells[index];
   }
 };
@@ -100,7 +150,8 @@ function run(level, program) {
       step(level, cmdName, arg0);
     } catch (ex) {
       if (ex) {
-        log('command failed! aborting...');
+        log(`ERROR: ${ex}`);
+        log('aborting program.');
         return;
       }
     }
